c188d5fcf4d24c147a06cd003f20457023841604
FernandaG <fernanda.guemez@gmail.com>
2024-01-22 18:13:44 -0600

    Se agrega calculator y los test de suma y resta

--- calculator.js
+++ calculator.js
@@ -1,18 +1,23 @@
 const calculator = {
-    sum(a, b) {
-        return a + b;
-    },
-    substract(a, b) {
-        return a - b;
-    },
-    multiply(a, b) {
-        return a * b;
-    },
-    divide(a, b) {
-        return a / b;
-    },
-    power: (a,b) => a / b,
-}
+  sum(a, b) {
+    return a + b;
+  },
+  substract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  power: (a, b) => a ^ b,
+};
 
+console.log("Suma: " + calculator.sum(5, 3)); // Imprime "Suma: 8"
+console.log("Resta: " + calculator.substract(10, 4)); // Imprime "Resta: 6"
+console.log("Multiplicación: " + calculator.multiply(2, 6)); // Imprime "Multiplicación: 12"
+console.log("División: " + calculator.divide(20, 5)); // Imprime "División: 4"
+console.log("Potencia: " + calculator.power(2, 3)); // Imprime "Potencia: 8"
 
 module.exports = calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,21 @@
-const { default: expect } = require('expect');
+const calculator = require("../calculator");
 
-const calculator = require('../calculator');
+test("Adds a + b to equal 3", () => {
+  expect(calculator.sum(5, 3)).toBe(8);
+});
 
-test('adds a + b', () => {
-    //
-})
+test("substracts a - b", () => {
+  expect(calculator.substract(10, 4)).toBe(6);
+});
 
-test('substracts a - b', () => {
-   //
-})
+// test("multiplies a * b", () => {
+//   //
+// });
 
-test('multiplies a * b', () => {
-    //
-})
+// test("divides a / b", () => {
+//   // Consider decimals
+// });
 
-test('divides a / b', () => {
-    // Consider decimals
-})
-
-test('Raises to power a ^ b', () => {
-    //
-})
-
+// test("Raises to power a ^ b", () => {
+//   //
+// });
